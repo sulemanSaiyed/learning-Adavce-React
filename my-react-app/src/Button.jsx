e4947a77f43425dbea67ab3778ab8fe71d024378
pandu-12-2000 <crackivar@gmail.com>
2025-09-24 18:44:39 +0530

    styling using inline styling

---
+++ my-react-app/src/Button.jsx
@@ -0,0 +1,21 @@
+
+
+
+function Button(){
+  const styless={
+ 
+      backgroundColor: "hsl(200, 100%, 50%)",
+      color: "white",
+      padding: "10px 20px",
+      borderRadius: "5px",
+      border: "none",
+      cursor: "pointer",
+    
+  }
+return(
+<button style={styless}>Click me</button>
+);
+
+
+}
+export default Button;
