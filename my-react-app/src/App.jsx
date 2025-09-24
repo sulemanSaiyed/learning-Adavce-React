8718489e68918ca9c4927218d4de7f439fe9016a
pandu-12-2000 <crackivar@gmail.com>
2025-09-24 20:48:03 +0530

    props intro

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,12 @@
-import Button from "./Button";
+import Student from "./Student";
 function App() {
   
   return(
     <> 
-<Button/>
+<Student name="Pandu" age={44} isStudent={true}/>
+<Student name="Mandu" age={46} isStudent={false}/>
+<Student name="Kandu" age={36} isStudent={false}/>
+<Student name="Sandu" age={86} isStudent={true}/>
 </>
   );
 }
