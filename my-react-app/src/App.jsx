49c0f2e86f4377b5adabec9d266e31caf508678c
pandu-12-2000 <crackivar@gmail.com>
2025-09-24 16:25:51 +0530

    we can write directly js varible outsdie return but while using inside return we should use { } braces we can use js methods also inside that

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,13 +1,14 @@
 
 import Header from "./Header"
 import Footer from "./Footer";
-
+import Food from "./Food ";
 
 function App() {
   
   return(
     <> 
 <Header/>
+<Food/>
 <Footer/>
 </>
   );
