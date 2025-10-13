0804bfdfde126b518d865ac380c24cd0f814d83c
pandu-12-2000 <crackivar@gmail.com>
2025-10-13 18:44:17 +0530

    update arrayof object in state

--- my-react-app/src/MyComponnet.jsx
+++ my-react-app/src/MyComponnet.jsx
@@ -1,39 +1,70 @@
+import { element } from "prop-types";
 import React,{useState} from "react";
 
 
 function MyComponnet(){
 
-const[food, setFoods]=useState(["Apple","Banana", "chiku"
-                          ]);
+const [cars, setCars]=useState([]);
+const [carYear, setCarYear]=useState(new Date().getFullYear());
+const [carMake, setCarMake]=useState("");
+const [carmodel, setModel]=useState("");
 
+function handelAddCar(){
+const newCar={year:carYear,
+         make:carMake, 
+       model:carmodel};
 
-function handleAddFood(){
-const newFood=document.getElementById("foodInput").value;
-document.getElementById("foodInput").value="";
-setFoods(f=>[...f,newFood])
+       setCars(c=>[...c, newCar]);
+
+       setCarYear(new Date().getFullYear());
+       setCarMake("");
+       setModel("");
 }
-function handleRemoveFood( index){
-setFoods(food.filter((_, i)=>i!==index))
-  
+
+function handelRemoveCar(index){
+
+setCars(c=>c.filter((_,ii)=>ii!==index));
 }
-return(
-  <>
 
-<div>
+function handelYearChange(e){
+  setCarYear(e.target.value);
 
-<h2>List of foods</h2>
+
+
+}
+function handelMakeChange(e){
+setCarMake(e.target.velue);
+}
+
+function handelModelhange(e){
+setModel(e.target.value);
+}
+
+return (
+  <div>
+<h2>List of car objects</h2>
 <ul>
-  {
-    food.map((food, index)=><li key={index}
-    onClick={()=>handleRemoveFood(index)}>{food}</li>)
-  }
+{cars.map((car,index)=>
+<li key={index} onClick={()=>handelRemoveCar(index)}>
+  {car.year}{car.make}{car.model}
+
+
+
+
+</li>
+
+
+)}
+
 </ul>
-<input type="text" id="foodInput" placeholder="enter food name"/ >
-<button onClick={handleAddFood}>Add food</button>
-</div>
+<input type="number"  value={carYear} onChange={handelYearChange}/><br />
+<input type="text"  value={carMake} onChange={handelMakeChange}
+placeholder=" enter car make"/><br />
+<input type="text"  value={carmodel} onChange={handelModelhange}
+placeholder=" enter car model"/><br /> 
+<button onClick={handelAddCar}>Add car</button>
+  </div>
+ );
 
-
-  </>
-)
 }
 export default MyComponnet;
